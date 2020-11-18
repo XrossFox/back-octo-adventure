9b18658a2d71508ffcd7b242d847ae0652be0824
David del Real <warhead_1090@hotmail.com>
2020-11-18 17:54:57 -0600

    Testing the first endpoint

---
+++ back-octo-adventure/Controllers/FieldMatrixController.cs
@@ -0,0 +1,25 @@
+﻿using back_octo_adventure.Models.ResponseWrapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_octo_adventure.Controllers
+{   
+    [ApiController]
+    [Route("[controller]")]
+    public class FieldMatrixController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> testCall() {
+
+            ResponseWrapper<int> Response = new ResponseWrapper<int>();
+            Response.body = 69;
+            Response.message = "Holis :D";
+
+            return Ok(Response);
+        }
+    }
+}
---
+++ back-octo-adventure/Models/ResponseWrapper/ResponseWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_octo_adventure.Models.ResponseWrapper
+{
+    public class ResponseWrapper<T>
+    {
+        public string message { get; set; }
+        public T body { get; set; }
+    }
+}
