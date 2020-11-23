a6dd087eb3e61fdacbc8dc913894513f820ee5fe
David del Real <warhead_1090@hotmail.com>
2020-11-23 16:08:58 -0600

    Added map.json so tileset generator can be mapped to a common set of elements

--- back-octo-adventure/Services/GenerateField/GenerateField.cs
+++ back-octo-adventure/Services/GenerateField/GenerateField.cs
@@ -1,4 +1,5 @@
 ﻿using back_octo_adventure.Models.FieldMatrix;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,18 @@ namespace back_octo_adventure.Services.GenerateField
 {
     public class GenerateField : IGenerateField
     {
+
+        private IConfiguration configurator;
+
+        /// <summary>
+        /// Base constructor receiver an IConfiguration instance to allow access to map.json config file.
+        /// </summary>
+        /// <param name="config"></param>
+        public GenerateField(IConfiguration config) {
+
+            configurator = config;
+        }
+
         public FieldGrid GenerateFieldGrid(int rows, int columns)
         {
             FieldGrid fGrid = new FieldGrid();
@@ -16,7 +29,7 @@ namespace back_octo_adventure.Services.GenerateField
 
             // if bool is false, then terrain will be stone, else will be dirt.
             Random random = new Random();
-            bool dirtOrStone = random.Next(0, 10) > 5 ? true: false;
+            bool floorTilesetType = random.Next(0, 1) == 0? true: false;
 
             for (int row = 0; row < fGrid.FieldGridArray.GetLength(0); row++) {
 
@@ -29,35 +42,35 @@ namespace back_octo_adventure.Services.GenerateField
                     {
                         // its the left one
                         if (col == 0)
-                            fGrid.FieldGridArray[row][col] = "CUL";
+                            fGrid.FieldGridArray[row][col] = configurator["CornerUpLeft"];
                         // its the right one
                         else
-                            fGrid.FieldGridArray[row][col] = "CUR";
+                            fGrid.FieldGridArray[row][col] = configurator["CornerUpRight"];
                         continue; // go-to next cycle jail, or add a tile where it shouldn't be 
                     }
 
                     // if it's a lower corner tile, either left or right
                     if ((row == rows - 1 && col == 0) || (row == rows - 1 && col == columns - 1))
-                    {   
+                    {
                         // left
                         if (col == 0)
-                            fGrid.FieldGridArray[row][col] = "CDL";
+                            fGrid.FieldGridArray[row][col] = configurator["CornerDownLeft"];
                         else
-                            fGrid.FieldGridArray[row][col] = "CDR";
-                        continue; 
+                            fGrid.FieldGridArray[row][col] = configurator["CornerDownRight"];
+                        continue;
                     }
 
                     // if it's the upper row, but not the corners
                     if ((row == 0) && (col > 0 && col < columns - 1))
                     {
-                        fGrid.FieldGridArray[row][col] = "UpB";
+                        fGrid.FieldGridArray[row][col] = configurator["BorderUpMid"];
                         continue; // go-to next cycle jail, or add a tile where it shouldn't be 
                     }
 
                     // if it's the lower row, but not the corners
                     if ((row == rows - 1) && (col > 0 && col < columns - 1))
                     {
-                        fGrid.FieldGridArray[row][col] = "LwB";
+                        fGrid.FieldGridArray[row][col] = configurator["BorderDownMid"];
                         continue; // go-to next cycle jail, or add a tile where it shouldn't be 
                     }
 
@@ -69,16 +82,17 @@ namespace back_octo_adventure.Services.GenerateField
                       )
                     {
                         if (col == 0) // left border
-                            fGrid.FieldGridArray[row][col] = "LfB";
+                            fGrid.FieldGridArray[row][col] = configurator["BorderLeft"];
                         else // right border
-                            fGrid.FieldGridArray[row][col] = "RgB";
+                            fGrid.FieldGridArray[row][col] = configurator["BorderRight"];
                         continue; // go-to next cycle jail, or add a tile where it shouldn't be 
                     }
 
                     // if you rechead this point, it means you are not anywhere in the upper row, lower row, first column nor last column
                     // so go full yolo and pick a tile randomly according ti tge dirtOrStone flag
-                    fGrid.FieldGridArray[row][col] = dirtOrStone ? "ST"+random.Next(0,5):"DR"+random.Next(0,5);
-
+                    fGrid.FieldGridArray[row][col] = floorTilesetType ?
+                        configurator["FloorTileSets:DirtTileSet:{tileN}".Replace("{tileN}", random.Next(1, 5).ToString())] :
+                        configurator["FloorTileSets:StoneTileSet:{tileN}".Replace("{tileN}", random.Next(1, 5).ToString())];
                 }
             }
 
